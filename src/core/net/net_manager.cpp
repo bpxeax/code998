99f193e6e0e1d0d7917a5778628ebc3563d4472b
bpxeax <bpxeax@gmail.com>
2016-07-28 18:34:56 +0800

    add net manager place holder

---
+++ src/core/net/net_manager.cpp
@@ -0,0 +1,6 @@
+#include "net_manager.h"
+
+namespace CoolMonkey
+{
+
+}
---
+++ src/core/net/net_manager.h
@@ -0,0 +1,12 @@
+#ifndef __COOLMONKEY_NET_MANAGER_H__
+#define __COOLMONKEY_NET_MANAGER_H__
+
+namespace CoolMonkey
+{
+    class NetManager
+    {
+
+    };
+}
+
+#endif
